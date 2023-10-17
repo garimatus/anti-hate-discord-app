1ad1766ec700ea5550ed8ea6881d87d6c7dbc9cd
garimatus <matus.edgar@gmail.com>
2023-10-17 00:02:04 -0300

    se agrega comando para modificar límite de advertencias per guild

---
+++ src/commands/moderation/limit.js
@@ -0,0 +1,50 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
+import { mapper } from "../../database/index.js";
+
+export const command = {
+	data : new SlashCommandBuilder()
+        .setName("limit")
+        .setDescription("Sets the limit value of allowed warnings per user at current Guild.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addStringOption(option => {
+			return option.setName("limit")
+				.setDescription("The new value of max warnings allowed.")
+				.setRequired(true)
+		}),
+	async execute(interaction) {
+		const newWarningsLimit = parseInt(interaction.options._hoistedOptions[0].value);
+
+        if (!Number(newWarningsLimit)) {
+            interaction.reply(`The new limit value isn't a number`);
+
+            return;
+        }
+
+        if (newWarningsLimit < 0) {
+            interaction.reply(`The new limit value cannot be less than zero`);
+
+            return;
+        }
+        
+        const antiHateBotMapper = mapper.forModel("Anti_Hate_Discord_Bot");
+		
+        const { warnings_allowed } = await antiHateBotMapper.get({
+            guild_id : interaction.member.guild.id
+        });
+        
+        if (newWarningsLimit == warnings_allowed) {
+            interaction.reply(`Error: the new limit value of warnings allowed in Guild cannot be the old one`);
+
+            return;
+        }
+
+		await antiHateBotMapper.update({
+			guild_id : interaction.member.guild.id,
+            warnings_allowed : newWarningsLimit
+		});
+
+		await interaction.reply(`
+			Guild's limit value of warnings per user setted from ${ warnings_allowed } to ${ newWarningsLimit }
+		`);
+	}
+};
