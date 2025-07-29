043c3dfe5992e9e2d4c4c9531ec6888c34191433
garimatus <matus.edgar@gmail.com>
2025-07-29 07:54:40 -0400

    se envuelve cuerpo de función en bloque try catch

--- src/events/utils/hateSpeechDetector.ts
+++ src/events/utils/hateSpeechDetector.ts
@@ -3,22 +3,27 @@ import { ollamaClient } from '../../ollama/index.js'
 import { type HateSpeechResponse } from '../../types/hate-speech-response.type.js'
 
 export default async function (message: string): Promise<HateSpeechResponse> {
-  const response: ChatResponse = await ollamaClient.generateChatResponse(
-    {
-      model: process.env.OLLAMA_API_MODEL || 'llama3.2',
-      messages: [
-        {
-          role: 'user',
-          content: message,
-        },
-      ],
-    },
-    'hate-speech-detection'
-  )
+  try {
+    const response: ChatResponse = await ollamaClient.generateChatResponse(
+      {
+        model: process.env.OLLAMA_API_MODEL || 'llama3.2',
+        messages: [
+          {
+            role: 'user',
+            content: message,
+          },
+        ],
+      },
+      'hate-speech-detection'
+    )
 
-  if (!response || !response.message || !response.message.content) {
-    throw new Error('Failed to get a valid response from the Ollama API')
+    if (!response || !response.message || !response.message.content) {
+      throw new Error('Failed to get a valid response from the Ollama API')
+    }
+
+    return JSON.parse(response.message.content) as HateSpeechResponse
+  } catch (error: any) {
+    console.error('Error in hate speech detection:', error)
+    return { result: false } as HateSpeechResponse
   }
-
-  return JSON.parse(response.message.content) as HateSpeechResponse
 }
