348c94821aaffd7c3b1f24cfdb960f8d35dc2762
garimatus <matus.edgar@gmail.com>
2025-08-06 15:53:42 -0400

    Se modifica lógica de inicio

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -2,8 +2,8 @@
 
 if [ $NODE_ENV = production ]; then
     ENV_FILE=.env;
-elif [ $NODE_ENV = local ]; then
-    ENV_FILE=.env.local;
-else
+elif [ $NODE_ENV = development ]; then
     ENV_FILE=.env.dev;
+else
+    ENV_FILE=.env.local;
 fi && node --env-file $ENV_FILE ./dist/index.js
